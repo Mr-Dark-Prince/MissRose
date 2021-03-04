d65fd74c3ca946298ac703a4e92ee53d65c0e0e5
MHD SALIM <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-05 00:18:33 +0530

    Initial Commit🍀

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "" # 🌚 Get it from my.telegram.org.
+    
